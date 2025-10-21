0683916d1458807eb390b2cbb95e090487f588d5
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-21 11:33:17 -0600

    Clase sistema lectura XML estructura

---
+++ Backed/Sistemas/SistemaLecturaXML.py
@@ -0,0 +1,26 @@
+from xml.dom.minidom import Document, parse, parseString
+
+from Clases.ArchivoConfiguracion import *
+
+
+class SistemaLeerArchivosXML:
+    def __init__(self):
+        self.ruta = None
+    
+    def msg(self, mensaje, extra):
+        print(f'[SistemaLeerArchivosXML]>> {mensaje}\n')
+        if extra != None and extra != '' and extra != ' ':
+            print('>> ',extra)
+    
+    def asignarruta(self, ruta):
+        self.ruta = ruta
+        self.msg('Ruta Asignada', ruta)
+
+    def leerArchivo(self):
+        if self.ruta == None:
+            self.msg('No hay ruta cargada',None)
+        else:
+            try:
+                pass
+            except Exception as e:
+                self.msg('Error: en leerArchivo()',e)
