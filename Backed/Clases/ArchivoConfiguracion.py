ec73cbeab0fa65f918554bec1ecee522685746a1
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-21 11:05:10 -0600

    clase recursos

--- Backed/Clases/ArchivoConfiguracion.py
+++ Backed/Clases/ArchivoConfiguracion.py
@@ -1,8 +1,21 @@
 
+########################################################
+
+class CRecurso():
+    def __init__(self,id,nombre,abreviatura,metrica,tipo,valorxhora):
+        self.id = id
+        self.nombre = nombre
+        self.abreviatura = abreviatura
+        self.metrica = metrica
+        self.tipo = tipo
+        self.valorxhora = valorxhora
+    
+    def desplegar(self):
+        print(f"ID: {self.id}, Nombre: {self.nombre}, Abreviatura: {self.abreviatura}, Metrica: {self.metrica}, Tipo: {self.tipo}, Valor x Hora: {self.valorxhora}")
 
 
 
-
+########################################################
 class CArchivoConfiguracion():
     def __init__(self):
         pass
