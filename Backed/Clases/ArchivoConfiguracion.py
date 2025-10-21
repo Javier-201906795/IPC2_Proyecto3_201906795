4ef937f6e8d99222076a426f4b2c1c687acf630a
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-21 11:12:39 -0600

    Clase Categoria e hijos
    Configuracion
    Recurso configuracion

--- Backed/Clases/ArchivoConfiguracion.py
+++ Backed/Clases/ArchivoConfiguracion.py
@@ -1,5 +1,5 @@
 
-########################################################
+#[A] #####################################################
 
 class CRecurso():
     def __init__(self,id,nombre,abreviatura,metrica,tipo,valorxhora):
@@ -12,6 +12,46 @@ class CRecurso():
     
     def desplegar(self):
         print(f"ID: {self.id}, Nombre: {self.nombre}, Abreviatura: {self.abreviatura}, Metrica: {self.metrica}, Tipo: {self.tipo}, Valor x Hora: {self.valorxhora}")
+
+#[B1]#######################################################
+
+class CCategoria():
+    def __init__(self, id, nombre, descripcion, cargatrabajo, listaconfiguracion):
+        self.id = id
+        self.nombre = nombre
+        self.descipcion = descripcion
+        self.cargatrabajo = cargatrabajo
+        self.listaconfiguracion = []
+    
+    def desplegar(self):
+        print(f"ID: {self.id}, Nombre: {self.nombre}, Descripcion: {self.descipcion}, Carga de Trabajo: {self.cargatrabajo}")
+        for configuracion in self.listaconfiguracion:
+            configuracion.desplegar()
+
+
+#[B2]#######################################################
+
+class CConfiguracion():
+    def __init__(self,id, nombre, descipcion, listarecursoconfiguracion):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descipcion
+        self.listarecursoconfiguracion = listarecursoconfiguracion
+
+    def desplegar(self):
+        print(f"ID: {self.id}, Nombre: {self.nombre}, Descripcion: {self.descripcion}")
+        for recurso in self.listarecursoconfiguracion:
+            recurso.desplegar()
+
+#[B3]#######################################################
+
+class CRecursoConfiguracion():
+    def __init__(self, id, cantidadrecurso):
+        self.id = id
+        self.cantidadrecurso = cantidadrecurso
+    
+    def desplegar(self):
+        print(f"ID: {self.id}, Cantidad Recurso: {self.cantidadrecurso}")
 
 
 
