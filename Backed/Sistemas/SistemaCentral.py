0041218ad7cd93feda0bd5a36c5399366bb82acc
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-21 12:02:40 -0600

    Clase sistema Central

--- Backed/Sistemas/SistemaCentral.py
+++ Backed/Sistemas/SistemaCentral.py
@@ -2,16 +2,32 @@ import os
 
 from SistemaLecturaXML import SistemaLeerArchivosXML
 
+class SistemaCentral():
+    def __init__(self):
+        self.SisLeerArhvXML = SistemaLeerArchivosXML()
+    
+    def LeerArchivo(self,ruta):
+        self.SisLeerArhvXML.asignarruta(ruta_archivo)
+        #Leer Archivo XML
+        self.SisLeerArhvXML.leerArchivo()    
+        print(self.SisLeerArhvXML.contenidoXML)
 
 
 
+
+
+
+####################################################################
 print('------[Sistema Central] -------')
+
 #Iniciar Sistema
-SisLeerArhvXML = SistemaLeerArchivosXML()
+SisCntr = SistemaCentral()
+
 #Cargar Ruta
 ruta_actual = os.getcwd()
 ruta_archivo = os.path.abspath(os.path.join(ruta_actual, '..', 'entrada.xml'))
-SisLeerArhvXML.asignarruta(ruta_archivo)
-#Leer Archivo XML
-SisLeerArhvXML.leerArchivo()    
-print(SisLeerArhvXML.contenidoXML)
+#Leer Archivo
+SisCntr.LeerArchivo(ruta_archivo)
+
+
+####################################################################
