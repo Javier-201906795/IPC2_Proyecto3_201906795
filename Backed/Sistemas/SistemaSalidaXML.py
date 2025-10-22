98a249d5294f7e00bbd7d3ade1074ded55c2c626
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-22 17:17:50 -0600

    funcion crear y guardar archivo configuraciones xml

--- Backed/Sistemas/SistemaSalidaXML.py
+++ Backed/Sistemas/SistemaSalidaXML.py
@@ -26,19 +26,33 @@ class SistemaSalidaXML():
         
         if self.rutasalida != None:
             self.msg('Guardando Archivo Configuraciones...')
-            print(self.rutasalida)
-            print(self.ArchivoConfig)
+            #Iniciar
+            self.creararchivoDOC()
+            #Segmentar
+            
+            #Guardar
+            self.crear_archivo()
+
         else:
             self.msg('No se ha asignado ruta salida')
         
     def creararchivoDOC(self):
         try:
-            print('Creando Doc and root')
+            self.msg('Creando Doc and root')
             self.doc = Document()
-            self.root = self.doc.createElement('datoSalida')
+            self.root = self.doc.createElement('archivoConfiguraciones')
             self.doc.appendChild(self.root)
         except Exception as e:
             self.msg("!!! Error al crear el archivo DOC!!!\n",e)
+    
+    def crear_archivo(self):
+        try:
+            contenido = self.doc.toprettyxml(indent=" ", encoding="UTF-8")
+            with open(self.rutasalida, 'wb') as archivo:
+                archivo.write(contenido)
+            self.msg(">> Archivo creado o sobrescrito correctamente.\n")
+        except Exception as e:
+            self.msg(f'!!! Error al crear_archivo !!!\n',e)
 
     def msg(self, mensaje, extra=None):
         print(f'[SistemaLeerArchivosXML]>> {mensaje}')
