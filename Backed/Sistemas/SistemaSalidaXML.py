fb6fe7dafed6bcc4ad7ca7861d2dfdeaacca9198
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-22 17:29:04 -0600

    funcion para añadir nodo hijo

--- Backed/Sistemas/SistemaSalidaXML.py
+++ Backed/Sistemas/SistemaSalidaXML.py
@@ -29,6 +29,7 @@ class SistemaSalidaXML():
             #Iniciar
             self.creararchivoDOC()
             #Segmentar
+            self.segmentar_archivo_XML()
 
             #Guardar
             self.crear_archivo()
@@ -36,108 +37,123 @@ class SistemaSalidaXML():
         else:
             self.msg('No se ha asignado ruta salida')
     
-    def segmentar_archivo_XML(self):
+    
+    def agregarhijo(self,hijo,padre):
         try:
             doc = self.doc
+
+            nodo = doc.createElement(hijo)
+            padre.appendChild(nodo)
+        except Exception as e:
+            self.msg('Error en agregarhijo()',e)
+    
+    def segmentar_archivo_XML(self):
+        try:
+            self.msg('segmentar archivo...')
+            doc = self.doc
             root = self.root
+
+            self.agregarhijo('listaRecursos',root)
+            # listaRecusos = doc.createElement('listaRecursos')
+            # root.appendChild(listaRecusos)
             
-            self.listaInvernaderos = doc.createElement('listaInvernaderos')
-            root.appendChild(self.listaInvernaderos)
+            # self.listaInvernaderos = doc.createElement('listaInvernaderos')
+            # root.appendChild(self.listaInvernaderos)
 
-            #Lista invernaderos
-            for i in range(0,self.colainvernaderos.tamano()):
-                Inv = self.colainvernaderos.Obtener(i+1)
-                #Datos invernadero
-                invernadero = doc.createElement('invernadero')
-                invernadero.setAttribute("nombre", f"{Inv.nombre}")
-                self.listaInvernaderos.appendChild(invernadero)
-                Inv.desplegar()
-                print()
-                print('> creando invernadero: ',Inv.nombre)
+            # #Lista invernaderos
+            # for i in range(0,self.colainvernaderos.tamano()):
+            #     Inv = self.colainvernaderos.Obtener(i+1)
+            #     #Datos invernadero
+            #     invernadero = doc.createElement('invernadero')
+            #     invernadero.setAttribute("nombre", f"{Inv.nombre}")
+            #     self.listaInvernaderos.appendChild(invernadero)
+            #     Inv.desplegar()
+            #     print()
+            #     print('> creando invernadero: ',Inv.nombre)
 
-                #Lista Planes
-                listaplanes = doc.createElement('listaPlanes')
-                invernadero.appendChild(listaplanes)
+            #     #Lista Planes
+            #     listaplanes = doc.createElement('listaPlanes')
+            #     invernadero.appendChild(listaplanes)
 
-                for h in range(0,Inv.ListaPlanes.tamano()):
-                    #Obtener informacion del historico movimientos
-                    Movmientos = Inv.historialmovimientos.Obtener(h+1)
-                    #Obtener informacion de Lista plantes
-                    Planinfo = Inv.ListaPlanes.Obtener(h+1)
-                    #Obtener tiempo optimo
-                    TiempooptimoInv = Inv.historiatiempooptimo.Obtener(h+1)
-                    #Obtener ferlitianzate y agua
-                    aguaopt = Inv.historiaagua.Obtener(h+1)
-                    fertopt = Inv.historaifertilizante.Obtener(h+1)
-                    #Obtener lista drones
-                    ListaDronesResumen = Inv.historialdrones.Obtener(h+1)
+            #     for h in range(0,Inv.ListaPlanes.tamano()):
+            #         #Obtener informacion del historico movimientos
+            #         Movmientos = Inv.historialmovimientos.Obtener(h+1)
+            #         #Obtener informacion de Lista plantes
+            #         Planinfo = Inv.ListaPlanes.Obtener(h+1)
+            #         #Obtener tiempo optimo
+            #         TiempooptimoInv = Inv.historiatiempooptimo.Obtener(h+1)
+            #         #Obtener ferlitianzate y agua
+            #         aguaopt = Inv.historiaagua.Obtener(h+1)
+            #         fertopt = Inv.historaifertilizante.Obtener(h+1)
+            #         #Obtener lista drones
+            #         ListaDronesResumen = Inv.historialdrones.Obtener(h+1)
                     
-                    print('\n---[Plan info]---')
-                    Planinfo.desplegar()
-                    print('---[Fin Plan info]---\n')
+            #         print('\n---[Plan info]---')
+            #         Planinfo.desplegar()
+            #         print('---[Fin Plan info]---\n')
 
-                    print('\n---[Movimientos]---')
-                    Movmientos.desplegar()
-                    print('---[Fin Movimientos]---\n')
+            #         print('\n---[Movimientos]---')
+            #         Movmientos.desplegar()
+            #         print('---[Fin Movimientos]---\n')
 
 
 
-                    #Datos Planes
-                    plan = doc.createElement('plan')
-                    plan.setAttribute('nombre',f'{Planinfo.nombre}')
-                    listaplanes.appendChild(plan)
+            #         #Datos Planes
+            #         plan = doc.createElement('plan')
+            #         plan.setAttribute('nombre',f'{Planinfo.nombre}')
+            #         listaplanes.appendChild(plan)
 
-                    #Tiempo optimo
-                    tiempoOptimo = doc.createElement('tiempoOptimoSegundos')
-                    plan.appendChild(tiempoOptimo)
-                    txt= doc.createTextNode(f'{TiempooptimoInv}')
-                    tiempoOptimo.appendChild(txt)
+            #         #Tiempo optimo
+            #         tiempoOptimo = doc.createElement('tiempoOptimoSegundos')
+            #         plan.appendChild(tiempoOptimo)
+            #         txt= doc.createTextNode(f'{TiempooptimoInv}')
+            #         tiempoOptimo.appendChild(txt)
 
-                    #aguaRequeridaLitros
-                    aguaRequerida = doc.createElement('aguaRequeridaLitros')
-                    plan.appendChild(aguaRequerida)
-                    txt= doc.createTextNode(f'{aguaopt}')
-                    aguaRequerida.appendChild(txt)
+            #         #aguaRequeridaLitros
+            #         aguaRequerida = doc.createElement('aguaRequeridaLitros')
+            #         plan.appendChild(aguaRequerida)
+            #         txt= doc.createTextNode(f'{aguaopt}')
+            #         aguaRequerida.appendChild(txt)
 
-                    #Fertilizante
-                    fertilizanteReq = doc.createElement('fertilizanteRequeridoGramos')
-                    plan.appendChild(fertilizanteReq)
-                    txt= doc.createTextNode(f'{fertopt}')
-                    fertilizanteReq.appendChild(txt)
+            #         #Fertilizante
+            #         fertilizanteReq = doc.createElement('fertilizanteRequeridoGramos')
+            #         plan.appendChild(fertilizanteReq)
+            #         txt= doc.createTextNode(f'{fertopt}')
+            #         fertilizanteReq.appendChild(txt)
 
-                    #Lista Eficiencia Drones
-                    efiDrones = doc.createElement('eficienciaDronesRegadores')
-                    plan.appendChild(efiDrones)
+            #         #Lista Eficiencia Drones
+            #         efiDrones = doc.createElement('eficienciaDronesRegadores')
+            #         plan.appendChild(efiDrones)
 
-                    #Dron
-                    for j in range(0,ListaDronesResumen.tamano()):
-                        DataDron = ListaDronesResumen.Obtener(j+1)
-                        Dron = doc.createElement('dron')
-                        efiDrones.appendChild(Dron)
-                        Dron.setAttribute('nombre',f'{DataDron.nombre}')
-                        Dron.setAttribute('litrosAgua',f'{DataDron.aguautilizada}')
-                        Dron.setAttribute('gramosFertilizante',f'{DataDron.fertilizanteutilizado}')
+            #         #Dron
+            #         for j in range(0,ListaDronesResumen.tamano()):
+            #             DataDron = ListaDronesResumen.Obtener(j+1)
+            #             Dron = doc.createElement('dron')
+            #             efiDrones.appendChild(Dron)
+            #             Dron.setAttribute('nombre',f'{DataDron.nombre}')
+            #             Dron.setAttribute('litrosAgua',f'{DataDron.aguautilizada}')
+            #             Dron.setAttribute('gramosFertilizante',f'{DataDron.fertilizanteutilizado}')
 
-                    #Lista instrucciones
-                    instrucciones = doc.createElement('instrucciones')
-                    plan.appendChild(instrucciones)
+            #         #Lista instrucciones
+            #         instrucciones = doc.createElement('instrucciones')
+            #         plan.appendChild(instrucciones)
 
-                    #Tiempo
-                    for k in range(0,Movmientos.tamano()):
-                        movdata = Movmientos.Obtener(k+1)
+            #         #Tiempo
+            #         for k in range(0,Movmientos.tamano()):
+            #             movdata = Movmientos.Obtener(k+1)
 
-                        tiempo = doc.createElement('tiempo')
-                        tiempo.setAttribute('segundos',f'{movdata.tiemposeg}')
-                        instrucciones.appendChild(tiempo)
+            #             tiempo = doc.createElement('tiempo')
+            #             tiempo.setAttribute('segundos',f'{movdata.tiemposeg}')
+            #             instrucciones.appendChild(tiempo)
 
-                        #Movimiento
-                        listamovimientos = movdata.colamovimientos
-                        for l in range(0,listamovimientos.tamano()):
-                            movi = listamovimientos.Obtener(l+1)
-                            movimiento = doc.createElement('dron')
-                            tiempo.appendChild(movimiento)
-                            movimiento.setAttribute('nombre',f'{movi.nombre}')
-                            movimiento.setAttribute('accion',f'{movi.accion}')
+            #             #Movimiento
+            #             listamovimientos = movdata.colamovimientos
+            #             for l in range(0,listamovimientos.tamano()):
+            #                 movi = listamovimientos.Obtener(l+1)
+            #                 movimiento = doc.createElement('dron')
+            #                 tiempo.appendChild(movimiento)
+            #                 movimiento.setAttribute('nombre',f'{movi.nombre}')
+            #                 movimiento.setAttribute('accion',f'{movi.accion}')
 
             
             
