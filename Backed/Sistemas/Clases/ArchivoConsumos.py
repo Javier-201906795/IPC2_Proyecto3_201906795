27d3139d026396f493e4064151a12f178a202e6b
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-23 15:12:54 -0600

    Crear Clase consumos

---
+++ Backed/Sistemas/Clases/ArchivoConsumos.py
@@ -0,0 +1,25 @@
+
+
+
+class CListaconsumos():
+    def __init__(self, Listaconsumos):
+        self.Listaconsumos = Listaconsumos
+    
+    def desplegar(self):
+        print('[Lista consumos]--')
+        if self.Listaconsumos != None or self.Listaconsumos != []:
+            for consumocliente in self.Listaconsumos:
+                consumocliente.desplegar()
+        print('[Fin Lista consumos]--')
+
+class CConsumoCliente():
+    def __init__(self, nitcliente, idinstancia, tiempo, fechahora):
+        self.nitcliente = nitcliente
+        self.idinstancia = idinstancia
+        self.tiempo = tiempo
+        self.fechahora = fechahora
+    
+    def desplegar(self):
+        print(f'   Nit Cliente: {self.nitcliente}, Id Instancia: {self.idinstancia}, Tiempo: {self.tiempo}, Fecha Hora: {self.fechahora}')
+        
+    
