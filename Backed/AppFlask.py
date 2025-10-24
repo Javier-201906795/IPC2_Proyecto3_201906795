5ed5a73da6eb99f4789b532a9cf0719d5dd58f3a
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-24 16:46:01 -0600

    Subir archivo fronten - Backend

--- Backed/AppFlask.py
+++ Backed/AppFlask.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 from flask_cors import CORS
 from flask.json import jsonify
 
+import os
 
 #Crear app
 app = Flask(__name__)
@@ -15,16 +16,35 @@ def inicio():
     except Exception as e:
         print('!!! Error FLASK inicio() !!!\n',e)
 
-@app.route('/subirConfig',   methods=['POST'])
+@app.route('/subirConfig', methods=['POST'])
 def subirConfig():
     try:
-        if request.method == 'POST':
-            if 'archivo' not in request.files:
-                mensaje = '!! No se encontro el archivo !! '
-                print(mensaje)
-            return 'hola'
+        mensaje = ''  # ✅ se inicializa correctamente
+
+        if 'archivo' not in request.files:
+            mensaje = '!! No se encontró el archivo !!'
+            print(mensaje)
+            return mensaje, 400
+
+        archivo = request.files['archivo']
+
+        if archivo.filename == '':
+            mensaje = '!! No se seleccionó ningún archivo !!'
+            print(mensaje)
+            return mensaje, 400
+
+        # Si no hubo errores, procesar el archivo
+        print('>>> Leyendo archivo...')
+        ruta_archivo = os.path.join(os.getcwd(), 'entrada.xml')
+        archivo.save(ruta_archivo)
+        print('>>> Se subió el archivo correctamente en:', ruta_archivo)
+
+        return 'Archivo Configuración recibido correctamente', 200
+
     except Exception as e:
-        print('!!! Error FLASK inicio() !!!\n',e)
+        print('!!! Error FLASK subirConfig() !!!\n', e)
+        return f'Error en el servidor Flask: {str(e)}', 500
+
 
 
 
