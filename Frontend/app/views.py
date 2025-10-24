e995052f4e45e8507e15774e4b7ab300a726cd94
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-24 16:33:01 -0600

    mandar archivo del frontend al bakcned

--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -19,25 +19,25 @@ def subirConfig(request):
         # URL del backend Flask
         flask_url = 'http://127.0.0.1:4000/subirConfig'  
 
-        # try:
-        #     # Enviar archivo al backend Flask
-        #     files = {'archivo': (archivo.name, archivo.read(), 'application/xml')}
-        #     response = requests.post(flask_url, files=files)
+        try:
+            # Enviar archivo al backend Flask
+            files = {'archivo': (archivo.name, archivo.read(), 'application/xml')}
+            response = requests.post(flask_url, files=files)
 
-        #     # Mostrar éxito o error según respuesta
-        #     if response.status_code == 200:
-        #         return render(request, 'subirConfig.html', {'banderaArchivo': True})
-        #     else:
-        #         return render(request, 'subirConfig.html', {
-        #             'banderaArchivo': False,
-        #             'error': f'Error desde el backend ({response.status_code}): {response.text}'
-        #         })
+            # Mostrar éxito o error según respuesta
+            if response.status_code == 200:
+                return render(request, 'subirConfig.html', {'banderaArchivo': True})
+            else:
+                return render(request, 'subirConfig.html', {
+                    'banderaArchivo': False,
+                    'error': f'Error desde el backend ({response.status_code}): {response.text}'
+                })
 
-        # except Exception as e:
-        #     return render(request, 'subirConfig.html', {
-        #         'banderaArchivo': False,
-        #         'error': f'Error de conexión con backend Flask: {str(e)}'
-        #     })
+        except Exception as e:
+            return render(request, 'subirConfig.html', {
+                'banderaArchivo': False,
+                'error': f'Error de conexión con backend Flask: {str(e)}'
+            })
 
     else:
         return render(request, 'subirConfig.html')
