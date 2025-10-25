4acdb743f890267dd10ec59d1ec46d94f51052de
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-24 23:39:02 -0600

    cambio orden
    para una mejor lectura

--- Frontend/app/urls.py
+++ Frontend/app/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('panelOpciones', views.panelOpciones, name='panelOpciones'),
     path('subirConfiguracion', views.subirConfig, name='subirConfig'),
     path('subirConsumos', views.subirConsumos, name='subirConsumos'),
-    path('panelOpciones', views.panelOpciones, name='panelOpciones'),
     path('operacionesSistema', views.operacionesSistema, name='operacionesSistema'),
     path('consultaPanel', views.consultaPanel, name='consultaPanel'),
     path('crearNuevoDato', views.crearNuevoDato, name= 'crearNuevoDat'),
